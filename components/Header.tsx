0234d8b3197580c9ee97f69f4324b9719050be89
cloer <chltjrrb97@gmail.com>
2022-06-24 13:06:10 -0400

    ADD: Header

---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='text-gray-600 body-font'>
+      <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
+        <a className='flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='2'
+            className='w-10 h-10 text-white p-2 bg-indigo-500 rounded-full'
+            viewBox='0 0 24 24'
+          >
+            <path d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5'></path>
+          </svg>
+          <span className='ml-3 text-xl'>Seokgyu Choi</span>
+        </a>
+        <nav className='md:ml-auto flex flex-wrap items-center text-base justify-center'>
+          <Link href='/'>
+            <a className='mr-5 hover:text-gray-900'>About</a>
+          </Link>
+          <Link href='/educations'>
+            <a className='mr-5 hover:text-gray-900'>Educations</a>
+          </Link>
+          <Link href='/projects'>
+            <a className='mr-5 hover:text-gray-900'>Projects</a>
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,17 @@
 import React, { ReactNode } from 'react';
+import Header from './header';
 
 interface LayoutPorps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutPorps) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
