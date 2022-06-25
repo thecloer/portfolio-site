f286ea20d89179919bfd77d8c55c221d42e2621e
cloer <chltjrrb97@gmail.com>
2022-06-25 06:33:43 -0400

    TEST

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 import Footer from './footer';
-import Header from './header';
+// import Header from './header';
 
 interface LayoutPorps {
   children: ReactNode;
@@ -9,7 +9,7 @@ interface LayoutPorps {
 const Layout = ({ children }: LayoutPorps) => {
   return (
     <div className='bg-white dark:bg-slate-800 flex flex-col min-h-full'>
-      <Header />
+      {/* <Header /> */}
       <main className='flex-1 container mx-auto px-5 py-16'>{children}</main>
       <Footer />
     </div>
