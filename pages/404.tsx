f62ebbac6a3c5f21ed0094594091108542c0cf45
cloer <chltjrrb97@gmail.com>
2022-07-05 18:42:36 -0400

    UPDATE: 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,22 @@
+import Head from 'next/head';
 import React from 'react';
+import Layout from '../components/layout';
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <Layout>
+      <Head>
+        <title>Not Found</title>
+        <meta name='description' content="Seokgyu Choi's portfolio" />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <div className='flex flex-wrap items-baseline sm:m-16'>
+        <h1 className='text-4xl font-medium border-r pr-2 mr-4'>404</h1>
+        <h3 className='text-2xl'>Not Found</h3>
+      </div>
+    </Layout>
+  );
 };
 
 export default NotFound;
