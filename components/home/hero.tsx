e5c0fd106da17e19754434af619b72b64b738dee
cloer <chltjrrb97@gmail.com>
2022-07-12 10:49:09 -0400

    .

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -6,9 +6,7 @@ const Hero = () => {
     <section className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
       <h1 className='sm:text-4xl text-3xl mb-14 font-medium text-gray-900'>Hi, I&apos;m Seokgyu Choi</h1>
       <p className='leading-relaxed text-xl'>
-        I&apos;m a student in the last year of my bachelor in geoinformatics in University of Seoul.
-        <br />
-        I&apos;m a full-stack web developer who loves Javascript ecosystem.
+        I&apos;m a junior full-stack web developer who loves Javascript ecosystem.
         <br />
       </p>
       <div className='mt-8 flex flex-col sm:flex-row'>
